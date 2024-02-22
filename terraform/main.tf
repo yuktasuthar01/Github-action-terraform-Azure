c2027ce9958cdd7f414573fabf815efb18678a1f
Yukta Suthar <yuktasuthar2001@gmail.com>
2024-02-22 10:47:09 +0530

    updated terraform.yml file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "Terraform-Test"
+    storage_account_name = "terraformyukta"
+    container_name = "tfstatefile"
+    key = "dev.terraform.tfstate"
+  }
+}
 module "rg" {
   source = "./modules/rg"
 

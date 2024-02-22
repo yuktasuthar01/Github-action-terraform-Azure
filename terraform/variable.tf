7799b17447f776210c99c5ae0833c860fb0943a4
Yukta Suthar <82398952+yuktasuthar01@users.noreply.github.com>
2024-02-22 11:25:17 +0530

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,15 @@
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "Azure Subscription ID"
+}
+
+variable "ARM_CLIENT_ID" {
+  description = "Azure Client ID"
+}
+
+variable "ARM_CLIENT_SECRET" {
+  description = "Azure Client Secret"
+}
+
+variable "ARM_TENANT_ID" {
+  description = "Azure Tenant ID"
+}

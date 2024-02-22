a9cfdc4aa7df463f9f95288daff8613a75d82935
Yukta Suthar <82398952+yuktasuthar01@users.noreply.github.com>
2024-02-22 11:08:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = "Terraform-Test"
+    resource_group_name = "terraform-test"
     storage_account_name = "terraformyukta"
     container_name = "tfstatefile"
     key = "dev.terraform.tfstate"

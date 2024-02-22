ea8678b1bbe473eacc01631716b037e89546a8b2
Yukta Suthar <yuktasuthar2001@gmail.com>
2024-02-22 12:25:09 +0530

    vnet added

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,18 @@ module "rg" {
   location            = "EastUS"
 
 }
+
+module "vnet" {
+  source               = "./modules/vnet"
+  location             = module.rg.location
+  resource_group_name  = module.rg.name
+  virtual_network_name = "vnet"
+  address_space        = ["10.0.0.0/16"]
+  dns_servers          = ["8.8.8.8", "8.8.4.4"]
+  subnets              = []
+
+  tags = {
+    environment = "dev"
+    project     = "my_project"
+  }
+}

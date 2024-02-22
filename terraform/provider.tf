bce2930a8317faf497cc9d4eb75698c70dd3c6c7
Yukta Suthar <82398952+yuktasuthar01@users.noreply.github.com>
2024-02-22 11:24:42 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,9 @@
 provider "azurerm" {
-  features {}
-  skip_provider_registration = true
+  features {}  # Optional: Enable specific provider features, if needed
+  
+  # Specify authentication details
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
 }
